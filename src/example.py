46a5ef3d99d87abbdb4c5c8fa11f4f497d443394
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2024-01-29 19:40:53 -0800

    include log level in example

--- src/example.py
+++ src/example.py
@@ -1,6 +1,9 @@
 from utility.palworld_util import PalworldUtil
 
 import os
+import sys
+
+from loguru import logger
 
 
 STEAMCMD_DIR = os.getenv("steamcmd_dir")
@@ -10,24 +13,32 @@ RCON_PASSWORD = os.getenv("palworld_rcon_password")
 RCON_PORT = int(os.getenv("palworld_rcon_port"))
 
 
+# Change level to DEBUG if you want more logging.
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+
 # Create PalworldUtil instance with required vars only.
 pal = PalworldUtil(STEAMCMD_DIR, SERVER_NAME, SERVER_IP, RCON_PORT, RCON_PASSWORD)
 
 # Don't rotate backups.
-pal.rotate_backups = False
+# pal.rotate_backups = False
 
 # or do...
-pal.rotate_backups = True
-pal.rotate_after_x_backups = 3  # Delete oldest backup after 3 backups.
+# pal.rotate_backups = True
+# pal.rotate_after_x_backups = 3  # Delete oldest backup after 3 backups.
 
-# Create a server backup.
-pal.take_server_backup()
+# # Create a server backup.
+# pal.take_server_backup()
 
-# Send an rcon command to the server.
-pal.rcon.run_command(command="Broadcast", args=["test"])
-
-# Send a broadcast to the server.
-pal.log_and_broadcast("Hello, world!")
+# # Send a broadcast to the server.
+# pal.log_and_broadcast("Server restart in 1 minute! Adding better network settings.")
 
 # Restart the server.
 # pal.restart_server(save_game=True, check_for_server_updates=True, backup_server=False)
+
+# Send rcon commands to the server
+response = pal.rcon.run_command("Info", [])
+logger.info(f"Info response: {response}")
+
+response = pal.rcon.run_command("ShowPlayers", [])
+logger.info(f"ShowPlayers response: {response}")
